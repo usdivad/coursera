29c9c51fe244366b92b24419a978fb1c0da133a9
David Su <david.d.su@gmail.com>
2014-02-17 23:16:19 -0500

    pmt when fv is known

--- introfin/formulae.py
+++ introfin/formulae.py
@@ -1,3 +1,5 @@
+# Implementing Excel functions
+
 # Week 1 (simple), 2 (annuity)
 def fv(rate, nper, pmt, pv=0, type="annuity"):
     if type=="simple":
@@ -16,3 +18,10 @@ def pv(rate, nper, pmt, fv=0, type="simple"):
         return fv / ((1+rate)**nper)
         
 # print pv(0.1, 2, 0, 121000) # 100,000
+
+
+def pmt(rate, nper, pv, fv=0, type="fv"):
+    if type=="fv":
+        return fv /((((1+rate)**nper)-1)/rate)
+        
+print pmt(.08, 25, 0, 500000) # 6839
