98fe1211a5bf049715865dc3066d0301816686dd
David Su <david.d.su@gmail.com>
2014-03-04 03:20:22 -0500

    got api search request response working yay

--- datasci/assignments/1/print.py
+++ datasci/assignments/1/print.py
@@ -2,6 +2,7 @@ import urllib2
 import urllib
 import json
 import base64
+import sys
 
 # Original methods deprecated!! booo
 # response = urllib.urlopen(url+"?q=microsoft?consumer_key=yXWiIYRi1QQ7rSWgRgFLMQ")
@@ -9,32 +10,71 @@ import base64
 # print json.load(response)
 
 # Getting credentials from external file
-creds = open("creds.txt", "r")
+# creds.txt in format "key\nsecret"
+def get_bearer():
+    creds = open("creds.txt", "r")
 
+    url = "https://api.twitter.com/1.1/search/tweets.json"
+
+    key = creds.readline().rstrip()
+    secret = creds.readline().rstrip()
+
+    creds.close()
+
+    # Encode as per Step 1 of https://dev.twitter.com/docs/auth/application-only-auth
+    encoded_value = base64.b64encode(key+":"+secret)
+
+    # Step 2: obtaining bearer token
+    header = {
+        "Authorization": "Basic " + encoded_value,
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    }
+
+    body = {
+        "grant_type": "client_credentials"
+    }
+
+    body_encoded = urllib.urlencode(body)
+
+    req = urllib2.Request("https://api.twitter.com/oauth2/token", body_encoded, header)
+
+    response = urllib2.urlopen(req)
+    bearer = json.load(response)['access_token']
+
+    return bearer
+
+
+
+# The actual API request haha
+# Search for command line argument
+args = sys.argv
+query = "microsoft"
+if len(args) > 1:
+    query = args[1]
+else:
+    query = "microsoft"
+    #print "Need at least 1 argo"
+    #exit()    
+
+# Setting up the query
+bearer = get_bearer()
 url = "https://api.twitter.com/1.1/search/tweets.json"
-
-key = creds.readline().rstrip()
-print key
-secret = creds.readline().rstrip()
-print secret
-
-creds.close()
-
-# Encode as per Step 1 of https://dev.twitter.com/docs/auth/application-only-auth
-encoded_value = base64.b64encode(key+":"+secret)
-
-# Step 2: obtaining bearer token
+data = {
+    "q": query,
+    "count": "10"
+}
+data_encoded = urllib.urlencode(data)
+url_encoded = url + "?" + data_encoded
 header = {
-    "Authorization": "Basic " + encoded_value,
-    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    "Authorization": "Bearer " + bearer
 }
+#print data_encoded
 
-body = {
-    "grant_type": "client_credentials"
-}
+# Sending the query
+req = urllib2.Request(url_encoded, headers=header)
+response = urllib2.urlopen(req)
 
-body_encoded = urllib.urlencode(body)
+# Getting the results
+results = json.load(response)
 
-req = urllib2.Request("https://api.twitter.com/oauth2/token", body_encoded, header)
-
-print urllib2.urlopen(req)
+print json.dumps(results, indent=4)
